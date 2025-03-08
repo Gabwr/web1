9f74e1c89a215a71f1880b886ce820720c93791d
Gabriel López <gamalop0104@gmail.com>
2025-03-08 17:37:19 -0500

    arreglo bloqueo por perfil

--- html/MenuInicio.php
+++ html/MenuInicio.php
@@ -22,6 +22,7 @@ if (!isset($_SESSION['usuario'])) {
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="../js/sesion.js"></script>
+    <script src="../js/restringirmenu.js"></script>
 </head>
 
 <body>
--- js/cambio_pagina.js
+++ js/cambio_pagina.js
@@ -1,35 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
   cargarPagina("inicio.php");
-  let user="";
-  let estate="";
-  if(sessionStorage.getItem("usuario")==null){
-    user=document.getElementById("usuario").innerHTML;
-    sessionStorage.setItem("usuario",user);
-  }else{
-    user = sessionStorage.getItem("usuario");
-  }
-  if(sessionStorage.getItem("estado")==null){
-    estate = document.getElementById("lestate").innerHTML;
-    sessionStorage.setItem("estado",estate);
-  }
-  $.ajax({
-    url: "../server/getpermisosperfil.php",
-    method: "POST",
-    data: { usuario: user},
-    success: function (response) {
-        try {
-            sessionStorage.setItem("perfil", response); 
-            setTimeout(restringir, 50);
-        } catch (e) {
-            console.log("Error al procesar los permisos");
-        }
-    },
-    error: function () {
-        alert("Error al obtener permisos desde el servidor");
-    }
-});
-
-
   document.body.addEventListener("click", function (event) {
       const link = event.target.closest("a");
       if (link && link.getAttribute("href") && !link.getAttribute("target")) {
@@ -99,49 +69,3 @@ function obtenerRutaRelativa(url) {
 }
 
 
-function restringir() {
-  let perfil = sessionStorage.getItem("perfil"); 
-  let leestate = sessionStorage.getItem("estado")
-  
-  if (!leestate) {
-    console.warn("No se encontró usuario en sessionStorage");
-    return;
-  }
-
-  //usuario inactivo
-  if(leestate=="Inactivo"){
-    sessionStorage.clear();
-    window.location.href = "inactivo.php";
-  }
-
-  if (!perfil) {
-      console.warn("No se encontró perfil en sessionStorage");
-      return;
-  }
-
-  perfil = JSON.parse(perfil);
-
-  if (perfil.lectura_ingresos != true && perfil.Insercion_ingresos_ != true && perfil.edicion_ingresos != true) {
-      document.getElementById("ingrperf").style.display = "none";
-
-  }
-  if (perfil.lectura_gastos != true && perfil.insercion_gastos != true && perfil.edicion_gastos != true) {
-      document.getElementById("gastperf").style.display = "none";
-  }
-
-  if (perfil.lectura_usuarios != true && perfil.insercion_usuarios != true && perfil.edicion_usuarios != true) {
-    document.getElementById("userperf").style.display = "none";
-  }
-
-  if (perfil.lectura_perfiles != true && perfil.insercion_perfiles != true && perfil.edicion_perfiles != true) {
-    document.getElementById("perfperf").style.display = "none";
-  }
-
-  if (perfil.lectura_conceptos != true && perfil.insercion_conceptos != true && perfil.edicion_conceptos != true) {
-    document.getElementById("cncpperf").style.display = "none";
-  }
-
-  if (perfil.permiso_qr != true ) {
-    document.getElementById("qrperf").style.display = "none";
-  }
-}
