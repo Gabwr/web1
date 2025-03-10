3cc388740c916092e68c1e5309d01ff0d4d08c0f
ImaGaf <oswaldoj.tipan@gmail.com>
2025-03-09 21:46:25 -0500

    Refactor

--- html/dashboard.php
+++ html/dashboard.php
@@ -119,23 +119,7 @@ $conn->close();
         </div>
     </div>
 
-    <script>
-        document.addEventListener("DOMContentLoaded", function () {
-            const monthInput = document.getElementById("month");
-
-            const savedMonth = localStorage.getItem("selectedMonth");
-            if (savedMonth) {
-                monthInput.value = savedMonth;
-            }
-
-            monthInput.addEventListener("change", function () {
-                localStorage.setItem("selectedMonth", monthInput.value);
-                window.location.href = "dashboard.php?month=" + monthInput.value;
-            });
-        });
-    </script>
+    <script src="../js/recuperarmonth.js"></script>
 
     <script src="../js/graficos.js"></script>
 </body>
-
-</html>  
